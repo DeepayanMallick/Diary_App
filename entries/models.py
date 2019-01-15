160b6947e31103982b3cc0f631153dde07696ffb
DeepayanMallick <deepayan1@live.com>
2019-01-15 16:45:42 +0600

    second commit

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Entry(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'entries'
